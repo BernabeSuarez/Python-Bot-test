bb858a6dc4f9419846ddf37653acf3cda07dc6b4
Bernabe <bernatattoo@gmail.com>
2023-11-20 20:02:42 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+from urllib import parse, request
+import re
+
+load_dotenv()
+
+token = os.getenv("TOKEN")
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} has connected to Discord!")
+
+
+@bot.command()
+async def hola(ctx):
+    # Saludo con nombre segun el autor del mensaje
+    await ctx.send(f"Welcome {ctx.message.author}")
+
+
+@bot.command()
+async def ayuda(ctx):
+    await ctx.send("Que quieres hacer:")
+    await ctx.send(
+        "Si queres ver un video de youtube: \n !youtube (Y el nombre del video que quieres ver)"
+    )
+
+
+@bot.command()
+async def youtube(ctx, *, search):
+    query_str = parse.urlencode({"search_query": search})
+    html_content = request.urlopen("https://www.youtube.com/results?" + query_str)
+    search_content = html_content.read().decode()
+    search_result = re.findall(r"\/watch\?v=\w+", search_content)
+    # print(search_result)
+
+    await ctx.send("https://www.youtube.com/" + search_result[0])
+
+
+bot.run(token)
